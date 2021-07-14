d3b392d8c3198d3222e920ecb3a49ac9029d5d03
Andru <79055950+andru17urdna@users.noreply.github.com>
2021-07-14 18:08:28 -0500

    Add files

---
+++ backend/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send("What's up buttercup?!");
+});
+
+module.exports = router;
