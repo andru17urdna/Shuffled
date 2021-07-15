7ce503b3ca062c974122249906a9fa83f95eed52
andru <dr3wwatkins@gmail.com>
2021-07-14 21:16:13 -0500

    Probably works.

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 
 const { environment } = require('./config');
@@ -37,13 +39,38 @@ app.use(express.json());
     );
 
 
-
-    // backend/app.js
-const routes = require('./routes');
-
-// ...
-
 app.use(routes); // Connect all the routes
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack,
+    });
+  });
 
 
 module.exports = app;
